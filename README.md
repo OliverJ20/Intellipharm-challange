58beacc9a084b31b6ae11b391332ede52623652d
Oliver Johnson <oliver.johnson@studionone.com.au>
2020-03-08 18:33:34 +1000

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Intellipharm-challange
+
+This challenge took about 6 hours total, not including environment setup and the link
+but including code tidy up, repo setup and manual testing to make sure docker and everything
+worked correctly.
+
+I strongly believe I have been able to complete all tasks stated except for the 5th UI task,
+which was to drill into the chart for further detail.
+
+I had not attempted charts before and this proved to be a unique challenge, which I thought would be 
+fairly simply turned out to be rather complex. I do believe I would of been able to achieve this task
+however it would of taken considerably more time, possible a day or so without assistance or guidance 
+to fully comprehend the abilities and means to complete it.
+
+I based this project of a number of others that I have done similar, and some areas might of proven
+to be overkill, however it is the setup and environment i felt most comfortable in delivering the tasks.
+
+# Prerequisites
+
+1. `node installed` - If node is not installed please download from here https://nodejs.org/en/download/
+2. `Docker Compose installed` - If docker is not installed please follow the instructions here https://docs.docker.com/compose/install/
+
+# Running Project
+
+1. Upon downloading the project the first step in the root of the project is to run `docker-compose up -d` this
+should spin up the required docker containers to allow the api and database to function
+
+2. Next you should `cd into the code` folder and run `npm install` this will install all the node packages
+used in this project to facilitate its function
+
+3. Once the packages have been installed simply run `npm start` in the code folder and it will spin up
+the node server where the site can be accessed.
+
+# Notes
+
+I will link here that the api can be accessed through http://localhost:1337/api/members.php as long
+as the docker container is spun up and working correctly. 
+
+Also in cases where i have redeployed the project to other repos docker maintained the sql database,
+If it hasn't then the easiest method to restore the database for this challenge is to go to
+http://localhost:13373/adminer.php this will take you to adminer where you can use the login credentials
+in the `docker-compose.yml` file under the db section. Once logged in, simply navigate to the api table and
+import members.sql file, which can be located in the `api` folder of the project.
+
--- api/members.php
+++ api/members.php
@@ -1,6 +1,4 @@
 <?php
-
-// Would normally store these in an env but for the purpose of the challenge I left them here
 $host = "db";
 $user = "root";
 $password = "qwertyui";
@@ -15,28 +13,36 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
 
-//// Access-Control headers are received during OPTIONS requests
-//if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-//
-//    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-//        // may also be using PUT, PATCH, HEAD etc
-//        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-//
-//    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-//        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-//
-//    exit(0);
-//}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        // may also be using PUT, PATCH, HEAD etc
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
 
 $con = mysqli_connect($host, $user, $password,$dbname);
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 
+
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+//echo "help";
+//echo $_GET['first'];
+//echo $_GET["firstname"];
+
+
+
 
 switch ($method) {
     case 'GET':
@@ -50,27 +56,41 @@ switch ($method) {
         $rowCount = $_GET["rowCount"];
         $sql = "select * from members where firstname LIKE '$firstname%' AND surname Like '$surname%' AND email LIKE '$email%' AND gender LIKE '$gender%' AND joined_date LIKE '$joined_date%' LIMIT $offset, $rowCount";
         break;
+    case 'POST':
+        $firstname = $_POST["firstname"];
+        $surename = $_POST["surname"];
+        $email = $_POST["email"];
+        $gender = $_POST["gender"];
+        $joined_date = $_POST["joined_date"];
+
+        $sql = "insert into contacts (firstname, surname, email, gender, joined_date) values ('$firstname', '$surename', '$email', '$gender', '$joined_date')";
+        break;
 }
+
+
 
 // run SQL statement
 $result = mysqli_query($con,$sql);
 
 // die if SQL statement failed
+echo $result;
+
 if (!$result) {
     http_response_code(404);
     die(mysqli_error($con));
 }
 
-//if ($method == 'GET') {
-//    if (!$id) echo '[';
-//    for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
-//        echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
-//    }
-//    if (!$id) echo ']';
-//} elseif ($method == 'POST') {
-//    echo json_encode($result);
-//} else {
-//    echo mysqli_affected_rows($con);
-//}
+if ($method == 'GET') {
+    if (!$id) echo '[';
+    for ($i=0 ; $i<mysqli_num_rows($result) ; $i++) {
+        echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+    }
+    if (!$id) echo ']';
+} elseif ($method == 'POST') {
+    echo json_encode($result);
+} else {
+    echo mysqli_affected_rows($con);
+}
+
 
 $con->close();
